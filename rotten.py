c234bc285d8da7dd2754af266f1789e6d2985706
Amy Catherine Pickens <pickenac@vmp748.vampire>
2016-04-25 13:31:36 -0500

    more work on rotten

--- rotten.py
+++ rotten.py
@@ -1,8 +1,31 @@
 import omdb
 import csv
+import re
 
 myfile = open("myfile", 'wb')
 wr = csv.writer(myfile, quoting=csv.QUOTE_ALL) 
+'''
+with open("movies.list") as f: 
+    movielist = list(csv.reader(f))
+test = open("testf", 'wb')
+writ = csv.writer(test, quoting=csv.QUOTE_ALL)
+writ.writerow(movielist)
+'''
+
+with open('movies.list', 'r') as f:
+    #lines = f.read().splitlines()
+    lines = []
+    for line in f: 
+        if line[0] == '"':
+            line = line.rstrip('\n')
+            movtitle = re.search('"(.*)"', line)
+            movtitle = movtitle.group(1)
+            movyear = line[line.find("(")+1:line.find(")")]
+            lines.append("'" + movtitle + "', " + movyear)
+
+with open('testf', 'w') as f2:
+    for item in lines: 
+        f2.write("%s\n" % item)
 
 mlist = [('True Grit', 1969), ('True Grit', 2010)]
 scorelist = [] 
