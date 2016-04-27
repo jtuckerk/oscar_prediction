d6c5f4d272abfb81b3a88dfaeda78cf29cc63172
Amy Catherine Pickens <pickenac@vmp748.vampire>
2016-04-26 22:23:12 -0500

    rotten more

--- rotten.py
+++ rotten.py
@@ -4,16 +4,8 @@ import re
 
 myfile = open("myfile", 'wb')
 wr = csv.writer(myfile, quoting=csv.QUOTE_ALL) 
-'''
-with open("movies.list") as f: 
-    movielist = list(csv.reader(f))
-test = open("testf", 'wb')
-writ = csv.writer(test, quoting=csv.QUOTE_ALL)
-writ.writerow(movielist)
-'''
 
 with open('movies.list', 'r') as f:
-    #lines = f.read().splitlines()
     lines = []
     for line in f: 
         if line[0] == '"':
@@ -21,26 +13,32 @@ with open('movies.list', 'r') as f:
             movtitle = re.search('"(.*)"', line)
             movtitle = movtitle.group(1)
             movyear = line[line.find("(")+1:line.find(")")]
-            lines.append("'" + movtitle + "', " + movyear)
-
+            try: 
+                movyear = int(movyear)
+            except ValueError: 
+                movyear = 0
+            lines.append((str(movtitle), int(movyear)))
+    #lines = set(lines)
+'''
 with open('testf', 'w') as f2:
     for item in lines: 
         f2.write("%s\n" % item)
-
-mlist = [('True Grit', 1969), ('True Grit', 2010)]
+'''
+mlist = lines[:6000] #[('True Grit', 1969), ('True Grit', 2000), ('True Grit', 2010)]
 scorelist = [] 
+#print mlist
+#movie = omdb.get(title='True Grit', year=1900, tomatoes=True)
+#print movie
 
-#movie = omdb.get(title='True Grit', year=1969, tomatoes=True)
 for item in mlist:
     movie = omdb.get(title=item[0], year=item[1], tomatoes=True)
-    mtitle = movie.title
-    myear = movie.year
-    critscore = movie.tomato_meter
-    conscore = movie.tomato_user_meter
-    mid = movie.imdb_id
-
-    print("(" + mid + ", " + mtitle + ", " + myear + ", " + critscore + ", " + conscore + ")")
-    scorelist.append(("(" + mid + ", " + mtitle + ", " + myear + ", " + critscore + ", " + conscore + "\
-)"))
-
+    if str(movie) != "Item({})":
+        mtitle = movie.title
+        myear = movie.year
+        critscore = movie.tomato_meter
+        conscore = movie.tomato_user_meter
+        mid = movie.imdb_id
+        if str(critscore) != "N/A":
+            scorelist.append(mid + ", " + mtitle + ", " + myear + ", " + critscore + ", " + conscore)
+            print("working")
 wr.writerow(scorelist)
