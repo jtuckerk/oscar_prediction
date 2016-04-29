28da9a4860e5bca18d1bd2f80771eb80cc24480f
James Tucker Kirven <kirvenjt@vmp744.vampire>
2016-04-28 20:00:11 -0500

    aight

--- rotten_parallel.py
+++ rotten_parallel.py
@@ -22,22 +22,37 @@ print "seems legit"
 
 def request(item):
 
-    movie = omdb.get(title=item[0], year=item[1], tomatoes=True)
+    try:
+        movie = omdb.get(title=item[0], year=item[1], tomatoes=True)
+    except Exception as e:
+        print e, "caused by", item[0]
+        return None
     if str(movie) != "Item({})":
         mtitle = movie.title
         myear = movie.year
         critscore = movie.tomato_meter
         conscore = movie.tomato_user_meter
         mid = movie.imdb_id
+        med_type =movie.type
+        runtime = movie.runtime
+        metascore= movie.metascore
+        lang = movie.language
+        released = movie.released
+        imdb_rating = movie.imdb_rating
+        imdb_votes = movie.imdb_votes
+        box_office = movie.box_office
+        country = movie.country
+
+
         if str(critscore) != "N/A":
-            return mid + " ||| " + mtitle + " |||  " + myear + " |||  " + critscore + " |||  " + conscore + "\n"
+            return mid + " ||| " + mtitle + " |||  " + myear + " |||  " + critscore + " |||  " + conscore + mid + " ||| " + med_type + " ||| " + runtime + " ||| " + metascore + " ||| " + lang + " ||| " + released + " ||| " + imdb_rating + " ||| " + imdb_votes + " ||| " + box_office + " ||| " + country + " ||| " + "\n"
         else:
             return None
  
-p = multiprocessing.Pool(32)
-
+p = multiprocessing.Pool(10)
+import sys
 def get_movies():
-    with codecs.open('./movies.list.clean/list.txt', 'r', encoding='utf-8') as f:
+    with codecs.open('./movies.list.clean/list0-299999.txt', 'r', encoding='utf-8') as f:
         lines = []
         for line in f.readlines(): 
 
@@ -51,12 +66,15 @@ def get_movies():
            except ValueError: 
                movyear = 0
            yield (unicode(movtitle), int(movyear))
-count =0 
+
+count =0
+import time
+start = time.time()
 for result in p.imap(request, get_movies()):
-    # (filename, count) tuples from worker
+    # (filename, count) tuples from worker                                                                                                                                         
     count +=1
     if result:
         outfile.write(result)
-    if count%5000==0:
-        print count
-
+    if count%500==0:
+        print count, int((time.time()-start))
+        sys.stdout.flush()
