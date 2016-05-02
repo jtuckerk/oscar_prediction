06babd21c847d33fad5cbae6b254015e0fb730a7
Amy Catherine Pickens <pickenac@vmp748.vampire>
2016-05-02 16:53:24 -0500

    more on rotten pred

--- rotten_prediction.py
+++ rotten_prediction.py
@@ -10,20 +10,7 @@ from sklearn.cross_validation import StratifiedKFold
 
 # player,year,stint,teamId,lgID,G,AB,R,H,2B,3B,HR,RBI,SB,CS,BB,SO,IBB,HBP,SH,SF,GIDP
 # omdb_responses: ['id','title','year','crit_score','cons_score',...]
-def load_rotten(): #def load_batting():
-    '''f = open('Batting.csv', 'r')
-    lines = f.readlines()
-    f.close()
-
-    batting = []
-    for line in lines:
-        line = line.strip()
-        if line[0] == '#':
-            continue
-        batting.append(line.split(','))
-
-    return batting
-    '''
+def load_rotten():
     f = open('omdb_responses/omdb_responses_full', 'r')
     lines = f.readlines()
     f.close()
@@ -32,28 +19,13 @@ def load_rotten(): #def load_batting():
     for line in lines: 
         line = eval(line)
         rotten.append(line)
-
+    
+    #print rotten
     return rotten
 
 # playerID,yearID,gameNum,gameID,teamID,lgID,GP,startingPos
 # oscar_y: (('title',year), num_wins_noms)
-def load_allstars():
-    '''
-    f = open('AllstarFull.csv', 'r')
-    lines = f.readlines()
-    f.close()
-
-    all_stars = {}
-    for line in lines:
-        line = line.strip().split(',')
-        if line[0] == '#':
-            continue
-        player = line[0]
-        year = line[1]
-        all_stars[(player, year)] = 1
-
-    return all_stars
-    '''
+def load_oscars(): 
     f = open('oscar_y/oscar_y_full', 'r')
     lines = f.readlines()
     f.close()
@@ -66,24 +38,33 @@ def load_allstars():
         if num >= 1: 
             oscars[(movie, str(year))] = 1
 
-    print oscars
     return oscars
 
 def load():
-    return load_rotten(), load_allstars()
+    return load_rotten(), load_oscars()
 
 
-def create_input(batting):
+def create_input(rotten): #def create_input(batting):
     # don't want to cinlude playerID, sting, team, league year in predicition
-    SKIP = 5
+    '''SKIP = 5
     WIDTH = len(batting[0]) - SKIP
     X = scipy.zeros((len(batting), WIDTH))
     for i in range(0, len(batting)):
         for j in range(SKIP, WIDTH):
                 X[i, j-SKIP] = batting[i][j] if batting[i][j] != '' else 0
     return X
-
-
+    '''
+    # id, title, year, critscore, conscore, media type, runtime, metascore, released, imdb rating, imdb votes, box office, country 
+    # only include year, critscore, conscore
+    SKIP = 3 
+    WIDTH = 2 # len(rotten[0]) - SKIP  
+    X = scipy.zeros((len(rotten), WIDTH))
+    for i in range(0, len(rotten)): 
+        for j in range(3,5): #(SKIP, WIDTH):
+            X[i, j-SKIP] = rotten[i][j] if rotten[i][j] != 'N/A' else 0
+    print X
+    return X 
+    
 def create_output(batting, all_stars):
     Y = scipy.zeros(len(batting))
     for i in range(0, len(batting)):
@@ -124,7 +105,8 @@ def main():
     clf = RandomForestClassifier(n_estimators=10, max_depth=10)
     test_classifier(clf, X, Y)
     '''
-    rotten, all_stars = load()
+    rotten, oscars = load()
+    X = create_input(rotten)
 
 if __name__ == '__main__':
     main()
