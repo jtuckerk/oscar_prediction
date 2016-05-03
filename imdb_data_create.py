dd985a753c81c85d44d9f6c9edfe58718f368ce3
James Tucker Kirven <kirvenjt@vmp744.vampire>
2016-05-03 12:19:04 -0500

    joining imdb with list of movies we care about

--- imdb_data_create.py
+++ imdb_data_create.py
@@ -20,20 +20,24 @@ an attempt at doing the rotten api requests from spark - too slow
 def create_y(spark):
     imdb_data = spark.textFile(imdb_file)
     movie_data = spark.textFile(all_movies)
-#    .keyBy(lambda x: (x[0], get_int_clean(x[1])))\
+    
+#            
     imdb_data = imdb_data.map(get_imdb_fields)\
-                         .filter(has_name_and_year)\
-                         .filter(is_weird)\
-                         .saveAsTextFile(test)
- #                        .map(lambda (x,y): (x,y,1))\
+                         .filter(lambda x: wrong_field_size(x, 5))\
+                         .keyBy(lambda x: (x[0], get_int_clean(x[1])))
+
 
     movie_data = movie_data.map(get_fields)\
-                           .filter(has_name_and_year)\
+                           .filter(lambda x: has_name_and_year(x[1], x[2]))\
                            .map(lambda x: (x[1], x[2]))\
                            .keyBy(lambda x: (x[0], get_int_clean(x[1])))\
+#((u'Brush with Danger', 2014), ((u'Brush with Danger', u'2014'), [u'Brush with Danger', u'2014', u'Stephanie', u'Hilbert', u'6']))
+    movie_data.join(imdb_data)\
+              .map(lambda (key, (key2,info)): tuple(info))\
+              .distinct()\
+              .saveAsTextFile(test)
 
-    c = movie_data.join(imdb_data).count()
-    print c
+
 '''
     movie_data = movie_data.map(get_fields)\
                            .map(lambda x: (x[1], x[2]))\
@@ -47,10 +51,10 @@ def create_y(spark):
               .reduceByKey(lambda x,y: x+y)\
               .saveAsTextFile(test)
 '''
-def is_weird(item):
-    return len(re.findall(r'\d{4}', item))==0
-def has_name_and_year(title_year):
-    return not (title_year[0] == "" or title_year[1]=="")
+def wrong_field_size(item, size):
+    return len(item) == size
+def has_name_and_year(title, year):
+    return not (title == "" or len(year)!=4)
 
 def encoding(item):
     if not item[1]:
@@ -60,9 +64,21 @@ def encoding(item):
     else:
         assert item[1][1] in actor_awards
         return 1
+def filter_broken_year(year):
+    try:
+        int(re.findall(r'\d{4}', item).pop())
+    except Exception as e:
+        return False
 
+    return True
 def get_int_clean(item):
-    return int(re.findall(r'\d{4}', item).pop())
+    try:
+        year =  int(re.findall(r'\d{4}', item).pop())
+    except Exception as e:
+        return 0
+
+    return year
+
 def p(item):
     print item
 
